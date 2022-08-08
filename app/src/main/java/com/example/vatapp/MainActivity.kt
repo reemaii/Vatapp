ae75655e88bf2bfedb9c7006137426501a3ff71f
reema <reema_abdulaziz@yahoo.com>
2022-08-08 13:46:00 +0300

    ...

--- app/src/main/java/com/example/vatapp/MainActivity.kt
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vatapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,8 +25,28 @@ class MainActivity : AppCompatActivity() {
 
     }// End onCreate
 
+    //@SuppressLint("StringFormatInvalid")
     private fun calculateVat() {
-        TODO("Not yet implemented")
+        val stringInTextField=binding.number.text.toString()
+        val cost  =stringInTextField.toDouble()
+        val SelectedId=binding.rgVatOption.checkedRadioButtonId
+        val vatpercentage= when (SelectedId){
+            R.id.rb_vat_10->0.10
+            R.id.rb_vat_15->0.15
+            else ->0.20
+        }
+
+        var vat = vatpercentage * cost
+        var total = cost + vat
+        // Switch value
+        val roundVat = binding.switchRoundUp.isChecked
+
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+        // Total Formatting
+        val formatedTotal = NumberFormat.getCurrencyInstance().format(total)
+        binding.txtCostTotal.text = getString(R.string.totall, formatedTotal)
     }
 
 }// End MainActivity
